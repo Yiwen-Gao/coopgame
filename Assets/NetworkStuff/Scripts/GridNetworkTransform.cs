df2a00dff8c8b4ce83283291d729a5549adc4346
Ryan Xu <ryan-xu@live.com>
2020-11-21 21:04:32 -0500

    Pushing networked movement code

---
+++ Assets/NetworkStuff/Scripts/GridNetworkTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class GridNetworkTransform : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(UpdatePosition))] public Vector2Int coords;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        coords = Vector2Int.zero;
+    }
+
+    void UpdatePosition(Vector2Int oldPos, Vector2Int newPos)
+    {
+        transform.position = new Vector3(newPos.x,newPos.y,0);
+    }
+}
--- Assets/NetworkStuff/Scripts/PlayerMovement.cs
+++ Assets/NetworkStuff/Scripts/PlayerMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
+    private GridNetworkTransform gnt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gnt = this.GetComponent<GridNetworkTransform>();
+        if (gnt == null)
+        {
+            throw new BadComponentException("This component requires a GridNetworkTransform on the object as well");
+        }
     }
 
     // Update is called once per frame
@@ -21,10 +26,16 @@ public class PlayerMovement : NetworkBehaviour
 
         if (Input.GetButtonDown("Horizontal"))
         {
-            transform.Translate(Vector3.right * (Input.GetAxis("Horizontal") > 0 ? 1 : -1));
+            CmdMove(Vector2Int.right * (Input.GetAxis("Horizontal") > 0 ? 1 : -1));
         } else if (Input.GetButtonDown(("Vertical"))) //we don't want diagonal movement (?)
         {
-            transform.Translate(Vector3.up * (Input.GetAxis("Vertical") > 0 ? 1 : -1));
+            CmdMove(Vector2Int.up * (Input.GetAxis("Vertical") > 0 ? 1 : -1));
         }
+    }
+
+    [Command]
+    void CmdMove(Vector2Int dir)
+    {
+        gnt.coords += dir;
     }
 }
