9aec4f35a43426402b3f729b4e3dcb55209b161c
yiwengao <yiwen.gao@columbia.edu>
2020-12-12 21:19:58 -0600

    change character size

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,26 +8,37 @@ public class Character : MonoBehaviour {
 	public Sprite body;
 	public Sprite tail;
 
-	public int blockSize = 5;
+	public int characterSize = 5;
+	public float blockSize = 25f;
 	public float speed = 0.1f;
+
+	private float[,] positions;
 
     // Start is called before the first frame update
     void Start() {
-        float[,] positions = CreateRep(); //new float[5,2]{{0,0}, {0,1}, {0,2}, {1,2}, {1,3}};
+        SetRepr(); //new float[5,2]{{0,0}, {0,1}, {0,2}, {1,2}, {1,3}};
 
         for (int i = 0; i < positions.GetLength(0); i++) {
         	GameObject block = new GameObject("Child");
         	SpriteRenderer sp = block.AddComponent<SpriteRenderer>();
-        	sp.sprite = head;
+        	if (i == 0) {
+        		sp.sprite = head;
+        	} else if (i == positions.GetLength(0) - 1) {
+        		sp.sprite = tail;
+        	} else {
+        		sp.sprite = body;
+        	}
+        	
+        	sp.drawMode = SpriteDrawMode.Sliced;
+        	sp.size = new Vector2(blockSize, blockSize);
         	block.transform.parent = transform;
-        	float spriteSize = 25f; //sp.sprite.rect.height;
-        	block.transform.position = new Vector3(positions[i,0] * spriteSize, positions[i,1] * spriteSize, 0f);
+        	block.transform.position = new Vector3(positions[i,0] * blockSize, positions[i,1] * blockSize, 0f);
         	Debug.Log(block.transform.position);
         }
     }
 
-    float[,] CreateRep() {
-    	float[,] positions = new float[blockSize,2]; 
+    void SetRepr() {
+    	positions = new float[characterSize,2]; 
     	int dif = 1;
     	positions[0,0] = positions[0,1] = 0;
     	for (int i = 1; i < positions.GetLength(0); i++) {
@@ -41,8 +52,6 @@ public class Character : MonoBehaviour {
 			}
 			Debug.Log(positions[i,0] + " " + positions[i,1]);
     	}
-
-    	return positions;
     }
 
     // Update is called once per frame
@@ -56,4 +65,8 @@ public class Character : MonoBehaviour {
     	Vector3 moveDir = new Vector3(xDir, yDir, 0f);
     	transform.position += moveDir * speed;
     }
+
+    float[,] GetPositions() {
+    	return positions;
+    } 
 }
